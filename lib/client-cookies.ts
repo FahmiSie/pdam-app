fe5aa04ee5fa47f3168e16c68a4ae91720e5e77e
Nama Anda <email@anda.com>
2026-01-22 07:22:59 +0700

    first push

---
+++ lib/client-cookies.ts
@@ -0,0 +1,14 @@
+import Cookies from "js-cookie";
+
+export const getCookie = (key: string) => {
+    return Cookies.get(key);
+
+}
+
+export const setCookie = (key: string, value: string) => {
+    return Cookies.set(key, value, { expires: 1});
+}
+
+export const removeCookie = (key: string) => {
+    return Cookies.remove(key);
+}
